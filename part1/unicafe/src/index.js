758e879b59bae3ac9d0b470feccd958bcc48d962
Sam Koh <samkoh4455@gmail.com>
2021-01-14 10:59:06 -0800

    exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Heading = (props) => <h1>{props.title}</h1>;
+
+const Results = ({rating, votes}) => <p>{rating}: {votes}</p>;
+
+const Button = (props) => {
+  return (
+    <button
+      onClick={props.handleClick}
+    >
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+
+  const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0});
+
+  const handleGoodClick = () => {
+    setFeedback({...feedback, good: feedback.good + 1 })
+  }
+
+  const handleNeutralClick = () => {
+    setFeedback({...feedback, neutral: feedback.neutral + 1})
+  }
+
+  const handleBadClick = () => {
+    setFeedback({...feedback, bad: feedback.bad + 1})
+  }
+
+  return (
+    <div>
+      <Heading title="give feedback"/>
+      <Button handleClick={() => handleGoodClick()} text="good"/>
+      <Button handleClick={() => handleNeutralClick()} text="neutral"/>
+      <Button handleClick={() => handleBadClick()} text="bad"/>
+      <Heading title="statistics"/>
+      <Results rating="good" votes={feedback.good}/>
+      <Results rating="neutral" votes={feedback.neutral}/>
+      <Results rating="bad" votes={feedback.bad}/>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
