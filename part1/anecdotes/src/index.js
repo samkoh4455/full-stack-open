246501bf66fb982ca434c0e2fb25f515e89eab2e
Sam Koh <samkoh4455@gmail.com>
2021-01-14 14:45:37 -0800

    exercise 1.12

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleClick = () => {
+    let idx = Math.floor(Math.random() * (props.anecdotes.length - 1));
+    if (idx >= selected) {
+      idx++;
+    }
+    console.log('selected: ', selected, 'idx: ', idx);
+    setSelected(idx);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        random
+      </button>
+      <p>
+        {props.anecdotes[selected]}
+      </p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
